1dff45f48e4fd273040eadc0a98b35f6d0a40a34
Varad-Dev-17 <Varad.Mule@cybaemtech.com>
2026-08-21 12:22:53 +0530

    fix

--- server/update_brands.cjs
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://varadmule17_db_user:YlGKnwW5vHWN1isK@vam.6n038fi.mongodb.net/ecommerce?appName=VAM')
-  .then(() => mongoose.connection.db.collection('brands').updateMany(
-    { slug: { $in: ['manyavar', 'peter-england', 'louis-philippe', 'raymond', 'nike', 'puma', 'adidas'] } }, 
-    { $addToSet: { departmentIds: new mongoose.Types.ObjectId('6a79a37e0ff9e1300720e3b5') } }
-  ))
-  .then(r => { console.log(r); process.exit(0); })
-  .catch(console.error);
--- server/update_brands.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/vyntra-ecommerce')
-  .then(() => mongoose.connection.db.collection('brands').updateMany(
-    { slug: { $in: ['manyavar', 'peter-england', 'louis-philippe', 'raymond', 'nike', 'puma', 'adidas'] } }, 
-    { $addToSet: { departmentIds: new mongoose.Types.ObjectId('6a79a37e0ff9e1300720e3b5') } }
-  ))
-  .then(r => { console.log(r); process.exit(0); })
-  .catch(console.error);
