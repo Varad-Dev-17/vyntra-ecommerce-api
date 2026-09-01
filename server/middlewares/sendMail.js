ed31ac1923e579fd4a6a4bab1fc0dee895034e57
Varad-Dev-17 <Varad.Mule@cybaemtech.com>
2026-09-01 21:40:28 +0530

    fix

--- server/middlewares/sendMail.js
+++ server/middlewares/sendMail.js
@@ -15,7 +15,12 @@ const transport = nodemailer.createTransport({
     user: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
     pass: process.env.NODE_CODE_SENDING_EMAIL_PASSWORD,
   },
-  family: 4,
+  // Enforce IPv4 lookup manually, overriding any Node/OS IPv6 preference
+  lookup: (hostname, options, callback) => {
+    dns.lookup(hostname, { family: 4 }, (err, address, family) => {
+      callback(err, address, family);
+    });
+  },
   connectionTimeout: 5000,
   greetingTimeout: 5000,
   socketTimeout: 5000,
